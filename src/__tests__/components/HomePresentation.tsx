857f26ef182ac00f3c23e30754a6af655430cc45
kmjak <tymkoron1221@icloud.com>
2025-04-04 23:38:22 +0900

    add: HomePresentationのtestコードを書いた

---
+++ src/__tests__/components/HomePresentation.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import HomePresentation from '@/components/HomePresentation';
+
+describe('Home Render', () => {
+  test('<HomePresentation /> の message にテキストを渡せる', () => {
+    const { getByRole } = render(<HomePresentation message="Hello Jest!!" />);
+    expect(getByRole('heading', { name: 'Hello Jest!!' })).toBeInTheDocument();
+  });
+});
